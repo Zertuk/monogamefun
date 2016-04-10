6c956ffc744d5f01a6e8448c1c05ba64efdfb850
Justin <zertukis@gmail.com>
2016-04-09 22:20:03 -0500

    init commit

---
+++ Game1/Game1.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private SpriteFont font;
+        private int score = 0;
+        private AnimatedSprite animatedSprite;
+        Vector2 position;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            position = new Vector2(graphics.GraphicsDevice.Viewport.Width / 2 - 64,
+                graphics.GraphicsDevice.Viewport.Height / 2 - 64);
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("Score");
+            Texture2D texture = Content.Load<Texture2D>("SmileyWalk");
+            animatedSprite = new AnimatedSprite(texture, 4, 4);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            KeyboardState state = Keyboard.GetState();
+
+            position = Player.Input(state, position);
+
+            // TODO: Add your update logic here
+
+            score++;
+
+            animatedSprite.Update();
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, "Score: " + score, new Vector2(100, 100), Color.Black);
+            spriteBatch.End();
+
+            animatedSprite.Draw(spriteBatch, position);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Game1/Player.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game1
+{
+    class Player
+    {
+        public static Vector2 Input(KeyboardState state, Vector2 position)
+        {
+            if (state.IsKeyDown(Keys.Right))
+            {
+                position.X += 5;
+            }
+            if (state.IsKeyDown(Keys.Left))
+            {
+                position.X -= 5;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                position.Y -= 5;
+            }
+            if (state.IsKeyDown(Keys.Down))
+            {
+                position.Y += 5;
+            }
+            return position;
+        }
+    }
+}
